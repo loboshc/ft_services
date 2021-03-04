096c84517d5430091a1fa1242c5242ef2c278cef
Diego <diegocomprar20@gmail.com>
2021-03-04 03:00:50 +0100

    phpymyadmin wordpress nginx

--- srcs/docker/phpmyadmin/srcs_phpmyadmin/init_phpmyadmin.sh
+++ srcs/docker/phpmyadmin/srcs_phpmyadmin/init_phpmyadmin.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+sleep 20
 #configuracion telegraf
 cd /sbin/telegraf/usr/bin
 ./telegraf -sample-config -input-filter cpu:mem:disk -output-filter influxdb > telegraf.conf
 sed -i '112s/.*/  urls = ["http:\/\/influxdb-svc:8086"]/' telegraf.conf
-sed -i '116s/.*/  database = "wordpress"/' telegraf.conf
+sed -i '116s/.*/  database = "phpmyadmin"/' telegraf.conf
 
-php --server 0.0.0.0:5000 --docroot /usr/share/webapps/phpmyadmin & sleep 20
+#configuracion nginx
+sed -i '3s/.*/user www;/' /etc/nginx/nginx.conf
+#configuracion openrc
+rc-status -a
+touch /run/openrc/softlevel
+#configuro usuario y grupo en PHP-FPM
+sed -i '23s/.*/user = www/' /etc/php7/php-fpm.d/www.conf
+sed -i '24s/.*/group = www/' /etc/php7/php-fpm.d/www.conf
+rc-service php-fpm7 start; rc-service nginx start
 ./telegraf --config telegraf.conf
--- srcs/docker/wordpress/srcs_wordpress/init.sh
+++ srcs/docker/wordpress/srcs_wordpress/init.sh
@@ -3,7 +3,7 @@
 cd /sbin/telegraf/usr/bin
 ./telegraf -sample-config -input-filter cpu:mem:disk -output-filter influxdb > telegraf.conf
 sed -i '112s/.*/  urls = ["http:\/\/influxdb-svc:8086"]/' telegraf.conf
-sed -i '116s/.*/  database = "phpmyadmin"/' telegraf.conf
+sed -i '116s/.*/  database = "wordpress"/' telegraf.conf
 
 sleep 20
 #configuracion nginx
